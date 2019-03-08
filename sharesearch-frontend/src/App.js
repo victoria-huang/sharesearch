391b87266b59c5ebc2c13b996fb359f8137d44a4
victoria-huang <vctrah@gmail.com>
2019-03-08 14:35:45 -0500

    testing ws

--- sharesearch-frontend/src/App.js
+++ sharesearch-frontend/src/App.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ActionCable from 'actioncable'
 
 class App extends Component {
+  state = {
+    bio: ''
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/profile', {
+      headers: {
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozfQ.m12gtrXTQ4P_jqoqM0tfx0lQes8daPfL9TC19vnAk7o'
+      }
+    })
+    .then(r => r.json())
+    .then(user => user.bio ? this.setState({ bio: user.bio }) : null )
+
+    const cable = ActionCable.createConsumer('ws://localhost:3000/cable')
+
+    this.sub = cable.subscriptions.create('BiosChannel', {
+      received: this.handleReceiveNewBio
+    })
+  }
+
+  handleReceiveNewBio = ({ bio }) => {
+    if (bio !== this.state.bio) {
+      this.setState({ bio })
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      bio: e.target.value
+    })
+
+    this.sub.send({ 
+      bio: e.target.value, 
+      id: 3 
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          <textarea onChange={ this.handleChange } value={ this.state.bio } />
         </header>
       </div>
     );
